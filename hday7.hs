721e45d8c65018cf45a69b75f45e7173573e982a
enikar <enikar@chezlefab.net>
2022-12-22 20:14:48 +0100

    Several improvements.
    Use a list of String to represent a Directory. That simplifies cmdLs and
    cmdCd functions, there is no need to split and join strings anymore.
    
    Also, cmdLs and cmdCd get their arguments in the same order.

--- hday7.hs
+++ hday7.hs
@@ -1,7 +1,7 @@
 -- 2022/12/07
 -- Solution of AoC 2022 7th day.
 
-import Data.List (intercalate, foldl')
+import Data.List (foldl')
 import Data.List.Extra (splitOn)
 
 import Data.Maybe (fromMaybe)
@@ -10,8 +10,10 @@ import Text.Read (readMaybe)
 import Data.Map.Strict (Map, (!), insert, singleton)
 import qualified Data.Map.Strict as M
 
-
-type Directory = String
+-- Directory is represented as a list of String.
+-- The root directory is ["/"], the directory /a is
+-- ["/", "a"].
+type Directory = [String]
 type Size = Int
 
 -- Hierarchy is the type which represents the file system
@@ -28,16 +30,11 @@ type Cmd = [String]
 -- CurrentState is a pair of current directory and the file system.
 type CurrentState = (Directory, Hierarchy)
 
--- XXX I should use a list for the current directory.
--- It'll be more handy to change directory.
--- And I could use also this list as a key for the Map.
--- it'll be less efficient but is it significant?
-
 main :: IO ()
 main = do
   datas <- readDatas "input.txt"
-  let rootFs = singleton "/" 0
-      fs = exeCmds ("/", rootFs) datas
+  let rootFs = singleton ["/"] 0
+      fs = exeCmds (["/"], rootFs) datas
   showSolution "Part1" (part1 fs)
   showSolution "Part2" (part2 fs)
 
@@ -51,7 +48,7 @@ part2 :: Hierarchy -> Int
 part2 fs = M.foldl' minimize rootSize fs
   where
     rootSize = 70000000
-    occupied = fs ! "/"
+    occupied = fs ! ["/"]
     freeSpace = rootSize - occupied
     needed = 30000000 - freeSpace
 
@@ -60,23 +57,24 @@ part2 fs = M.foldl' minimize rootSize fs
       |otherwise   = acc
 
 
--- Add directories found in listing to the Hierarchy.
+-- Add directories found in the listing to the Hierarchy.
 -- Adjust the size of the current directory by accumulating
 -- size of simple file of the listing.
-cmdLs :: CurrentState -> [String] -> CurrentState
-cmdLs (dir, fs0) listing = (dir, foldl' go fs0 listing)
+cmdLs :: [String] -> CurrentState -> CurrentState
+cmdLs listing (dir, fs0) = (dir, foldl' readEntry fs0 listing)
   where
     readInt s = fromMaybe (errReadInt s) (readMaybe s)
     errReadInt s = error ("CmdLs: Can't parse an Int: " <> s)
 
-    insertDir fs new = insert (intercalate "/" [dir, new]) 0 fs
+    mkDir fs new = insert  (dir <> new) 0 fs
 
     incSize fs size = insert dir ((fs ! dir) + size) fs
 
-    go fs s = let [dn, name] = words s
-              in case dn of
-                   "dir" -> insertDir fs name
-                   _     -> incSize fs (readInt dn)
+    readEntry fs s =
+      let [dn, name] = words s
+      in case dn of
+           "dir" -> mkDir fs [name]
+           _     -> incSize fs (readInt dn)
 
 -- execute a cd.
 -- When going to the parent. Add the size of the current dir to
@@ -85,29 +83,29 @@ cmdLs (dir, fs0) listing = (dir, foldl' go fs0 listing)
 cmdCd :: String -> CurrentState -> CurrentState
 cmdCd new (dir0, fs0) =
   case new of
-    ".." -> let dir = intercalate "/" (init (splitOn "/" dir0))
+    ".." -> let dir = init dir0
                 size0 = fs0 ! dir0
                 size = fs0 ! dir
             in
               (dir, insert dir (size+size0) fs0)
-    _    -> (intercalate "/" [dir0, new], fs0)
+    _    -> (dir0 <> [new], fs0)
 
 -- roll up to "/" and accumulate size of each traversed directory
 -- to its parent.
 reportSize :: CurrentState -> Hierarchy
 reportSize = snd . until satisfy improve
   where
-    satisfy (dir, _) = dir == "/"
+    satisfy (dir, _) = dir == ["/"]
     improve  = cmdCd ".."
 
--- build the file system into a Hierarchy
+-- build the file system as a Hierarchy
 exeCmds :: CurrentState -> [Cmd] -> Hierarchy
 exeCmds (dir0, fs0) = reportSize . foldl' exeOneCmd (dir0, fs0)
   where
     exeOneCmd (dir, fs) cmd =
       let ([c], ls) = splitAt 1 cmd
       in case c of
-              "ls" -> cmdLs (dir, fs) ls
+              "ls" -> cmdLs ls (dir, fs)
               _    -> let [_, dn] = words c -- this is a cd
                       in  cmdCd dn (dir, fs)
 
